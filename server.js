989da0400c03bd8076b4614df1fdd1676d108978
Vllaadd <vlladdiimiir@gmail.com>
2023-01-03 01:06:18 -0500

    route for registering users created

--- server.js
+++ server.js
@@ -21,15 +21,19 @@ app.get('/register', async(req, res) => {
 
 app.post('/register', async(req, res) => {
     try{
-
+        const hashedPassowrd = await bcrypt.hash(req.body.password, 10)
+        users.push({
+            id: Date.now().toString(),
+            name: req.body.name,
+            email: req.body.email,
+            password: hashedPassowrd
+        })  
+        res.redirect('/login');  
     }catch{
-
+        res.redirect('/register')
     }
-    req.body.email
+    console.log(users);
 })
 
-app.post('/register', (req, res) => {
-
-})
 
 app.listen(3000);

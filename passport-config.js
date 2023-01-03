a770bb050a2017b7e040e58cf6e72607d103a0e4
Vllaadd <vlladdiimiir@gmail.com>
2023-01-03 02:33:05 -0500

    authenticateUser for email and password

---
+++ passport-config.js
@@ -0,0 +1,26 @@
+const localStrategy = require('passport-local').Strategy
+const bcrypt = require('bcrypt');
+
+
+
+function initialize(passport, getUserByEmail){
+    authenticateUser = async(email, passport, done) => {
+        const user = getUserByEmail(email)
+        if(user == null){
+            return done(null, false, { message: 'no user with that email'})
+        }
+
+        try{
+            if(await bcrypt.compare(password, user.password)){
+                return done(null, user)
+            }else{
+                return done(null, false, {message: 'Password incorrect'})
+            }
+        }catch(e){
+            return done(e)
+        }
+    }
+    passport.use(new localStrategy({usernameField: 'email'}), authenticateUser)
+    passport.serializeUser((user, done) =>{})
+    passport.deserializeUser((id, done)=>{})
+}

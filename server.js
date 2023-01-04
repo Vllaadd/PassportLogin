cf28d3950aa1cd5229f686b2f005b2a877f7e58d
Vllaadd <vlladdiimiir@gmail.com>
2023-01-04 01:23:56 -0500

    logout created

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const bcrypt = require('bcrypt');
 const passport = require('passport');
 const flash = require('express-flash');
 const session = require('express-session');
+const methodOverride = require('method-override');
 
 const initializePassport = require('./passport-config');
 initializePassport(
@@ -28,6 +29,7 @@ app.use(session({
 }))
 app.use(passport.initialize())
 app.use(passport.session()) 
+app.use(methodOverride('_method'))
 
 app.get('/', checkAuthenticated, (req, res) => {
     res.render('index.ejs')
@@ -64,8 +66,8 @@ app.post('/register', checkNotAuthenticated , async(req, res) => {
 })
 
 app.delete('/logout', (req, res) => {
-    req. logOut()
-    req.redirect('/login');
+    req.logOut()
+    res.redirect('/login');
 })
 
 function checkAuthenticated(req, res, next){
